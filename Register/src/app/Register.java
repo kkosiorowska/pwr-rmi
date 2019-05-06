5efdebe7d7b2dc884fb1e9918edc31caaf9eb710
Karolina kosiorowska <kaarolina.kosiorowska@gmail.com>
2019-05-06 16:45:04 +0200

    Override methods for Register

--- Register/src/app/Register.java
+++ Register/src/app/Register.java
@@ -5,13 +5,37 @@ import java.util.ArrayList;
 
 public class Register  implements IRegister{
 
+    private ArrayList<OS> registeredServers = new ArrayList<>();
+
+    public Register(){
+
+    }
+
     @Override
     public boolean register(OS os) throws RemoteException {
+        if(os!=null)
+        {
+            System.out.println("Registered: " + os.getName());
+            registeredServers.add(os);
+            // contains – jako parametr przyjmuje element listy i zwraca flagę informującą czy dany element już istnieje
+            if(registeredServers.contains(os)) return true;
+        }
         return false;
     }
 
     @Override
     public ArrayList<OS> getServers() throws RemoteException {
-        return null;
+        return registeredServers;
+    }
+
+    public void showAllServers() throws RemoteException {
+        for(int j=0;j< getServers().size();j++)
+        {
+            System.out.println(getServers().get(j).getName());
+        }
+    }
+
+    public static void main(String[] args){
+        Register register = new Register();
     }
 }
